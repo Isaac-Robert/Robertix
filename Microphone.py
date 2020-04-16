e99f9b5dd498dea5695f8102c50e59c466fe516c
Isaac-Robert <58575083+Isaac-Robert@users.noreply.github.com>
2020-04-16 13:14:26 +0000

    Update Microphone.py

--- Microphone.py
+++ Microphone.py
@@ -1,1 +1,14 @@
-This is a microphone project
+import androidhelper
+import time
+
+droid = androidhelper.Android()
+name = str(input("📁File name: "))
+Tm = int(input("⏲️Recording Time(s): "))
+
+droid.recorderStartMicrophone("/storage/emulated/0/"+name+".mp3")
+print("Recording Start...")
+time.sleep(Tm)
+
+droid.recorderStop()
+print(" RECORDING STOPPED⏹️")
+droid.makeToast("File location: /storage/emulated/0/"+name+".mp3")
